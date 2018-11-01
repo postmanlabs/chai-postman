52a34da37f851cf0b4bc5686e6a12f1f9e2aeb10
pavantejap <pavan.teja@getpostman.com>
2018-11-01 16:37:16 +0530

    add system test fot .gitattributes

--- test/system/repository.test.js
+++ test/system/repository.test.js
@@ -184,6 +184,16 @@ describe('project repository', function () {
         });
     });
 
+    describe('.gitattributes', function () {
+        it('must exist', function (done) {
+            fs.stat('./.gitattributes', done);
+        });
+
+        it('must have readable content', function () {
+            expect(fs.readFileSync('./.gitattributes').toString()).to.be.ok;
+        });
+    });
+
     describe('.nsprc', function () {
         it('must exist', function (done) {
             fs.stat('./.nsprc', done);
